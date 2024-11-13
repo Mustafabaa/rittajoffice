c512248fb8e25ee9cd1f13d8b28ac0718fae261f
Mustafabaa <166011443+Mustafabaa@users.noreply.github.com>
2024-11-13 10:24:26 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCjHvHwlbPejTpcuXNscVbXOHht0SoF98M",
+  authDomain: "estatement-81a77.firebaseapp.com",
+  projectId: "estatement-81a77",
+  storageBucket: "estatement-81a77.firebasestorage.app",
+  messagingSenderId: "661540457267",
+  appId: "1:661540457267:web:bda7ec2d44708981638c46",
+  measurementId: "G-8G9HXY8Y8C"
+};
+
+firebase.initializeApp(firebaseConfig);
